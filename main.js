510ea6a8fd9830b5af799c1d0ed26a32e9bcb7e2
Jaron Johnson <jjaronjohnson14@gmail.com>
2020-08-10 09:40:48 -0600

    pushing to push...

--- main.js
+++ main.js
@@ -65,26 +65,28 @@ csv.forEach(user => {
         })
 })
 
-describe('Login and pull Cases ID', function () {
-    var driver;
+// This is where it breaks....
 
-    before(async function () {
-        driver = await new Builder().forBrowser('chrome').build();
-    });
+// describe('Login and pull Cases ID', function () {
+//     var driver;
 
-    console.log("made it");
-    it('Pull up Canvas instance ', async function () {
-        await driver.get(`https://${user.domain}/login/canvas`);
-        await driver.wait(until.elementLocated(By.className('ic-Input text')), 100000);
-        await driver.findElement(By.id('pseudonym_session_unique_id')).sendKeys(user.uniqueLogin);
-        await driver.findElement(By.id('pseudonym_session_password').sendKeys(user.password));
-        await driver.findElement(By.css("button[class='ic-Form-control ic-Form-control--login']")).click();
-        await driver.wait(until.elementLocated(By.className('ic-Dashboard-header__title')), 100000);
-        console.log("made it");
-        // await driver.get(`http://adminconsole.canvaslms.com`);
-    });
-    // removeLogins(user, instance);
-});
+//     before(async function () {
+//         driver = await new Builder().forBrowser('chrome').build();
+//     });
+
+//     console.log("made it");
+//     it('Pull up Canvas instance ', async function () {
+//         await driver.get(`https://${user.domain}/login/canvas`);
+//         await driver.wait(until.elementLocated(By.className('ic-Input text')), 100000);
+//         await driver.findElement(By.id('pseudonym_session_unique_id')).sendKeys(user.uniqueLogin);
+//         await driver.findElement(By.id('pseudonym_session_password').sendKeys(user.password));
+//         await driver.findElement(By.css("button[class='ic-Form-control ic-Form-control--login']")).click();
+//         await driver.wait(until.elementLocated(By.className('ic-Dashboard-header__title')), 100000);
+//         console.log("made it");
+//         // await driver.get(`http://adminconsole.canvaslms.com`);
+//     });
+//     // removeLogins(user, instance);
+// });
 
 // Set up as Field Admin
 
